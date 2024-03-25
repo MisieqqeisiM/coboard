516237e833d95e7320d9b6da86d7a54d9a140d42
Michał Pajda <gasosaur@gmail.com>
2024-03-26 00:21:13 +0100

    added ssl

--- liaison/server.ts
+++ liaison/server.ts
@@ -1,4 +1,5 @@
 import { Server, Socket } from "$socketio/mod.ts";
+import { cert, key } from "../certificates/certificates.ts";
 import { Server as ServerLogic } from "../server/server.ts"
 import { ClientToServerEvents, ServerToClientEvents } from "./liaison.ts";
 
@@ -6,10 +7,11 @@ export type SocketServer = Server<ClientToServerEvents, ServerToClientEvents, {}
 export type ServerSocket = Socket<ClientToServerEvents, ServerToClientEvents, {} , {}>;
 
 export function createServer(): ServerLogic {
-  const io: SocketServer = new Server();
+  const io: SocketServer = new Server({});
   const server = new ServerLogic(io);
   const handler = io.handler();
-  Deno.serve({ port: 3690 }, (req, info) => handler(req, {
+
+  Deno.serve({ port:3690, cert: cert(), key: key() } as Deno.ServeTlsOptions, (req, info) => handler(req, {
     localAddr: { transport: "tcp", hostname: "localhost", port: 3690 },
     remoteAddr: info.remoteAddr
   }));
